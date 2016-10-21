08445aa70f12023ab9a884f73043070a70700530
Wingo <cmwingo@gmail.com>
2016-10-21 09:50:52 -0700

    Now pass survey answers as an array correctly

--- js/scripts.js
+++ js/scripts.js
@@ -8,29 +8,37 @@ $(document).ready(function (){
     var question3Answer = $("input:radio[name=question3]:checked").val();
     var question4Answer = $("input:radio[name=question4]:checked").val();
     var question5Answer = $("input:radio[name=question5]:checked").val();
+    var answers = [question1Answer, question2Answer, question3Answer, question4Answer, question5Answer];
     console.log(question1Answer);
     console.log(question2Answer);
     console.log(question3Answer);
     console.log(question4Answer);
     console.log(question5Answer);
-    var result = findPath(question1Answer, question2Answer, question3Answer, question4Answer, question5Answer);
+    console.log(answers[0]);
+    console.log(answers[1]);
+    console.log(answers[2]);
+    console.log(answers[3]);
+    console.log(answers[4]);
+    // var result = findPath(question1Answer, question2Answer, question3Answer, question4Answer, question5Answer);
+    var result = findPath(answers);
     console.log(result);
   });
 });
 
 //Business Logic
-var findPath = function(ans1, ans2, ans3, ans4, ans5){
+var findPath = function(answers){
   var tr1 = 0;
   var tr2 = 0;
   var tr3 = 0;
   var tr4 = 0;
   var tr5 = 0;
   var idk = 0;
-  var answer1 = parseInt(ans1);
-  var answer2 = parseInt(ans2);
-  var answer3 = parseInt(ans3);
-  var answer4 = parseInt(ans4);
-  var answer5 = parseInt(ans5);
+  var answer1 = parseInt(answers[0]);
+  var answer2 = parseInt(answers[1]);
+  var answer3 = parseInt(answers[2]);
+  var answer4 = parseInt(answers[3]);
+  var answer5 = parseInt(answers[4]);
+  var track;
 
   if(answer1 === 1){
     tr3++;
@@ -97,6 +105,13 @@ var findPath = function(ans1, ans2, ans3, ans4, ans5){
   } else if (answer4 === 4){
     idk++;
   }
+
+  // for(i = 0; i < 5; i++){
+  //   debugger;
+  //   if("'tr' + '(i+1)' " > " 'tr' + '(i+2)' "){
+  //     track = "tr" + i;
+  //   }
+  // }
 
 
   console.log(tr1);
